3316f2e58b7efda6ba8a68cd55dd09581a1d5690
Hymin2 <khm8246@gmail.com>
2024-01-09 15:01:16 +0900

    feat: 이벤트 상세 화면에서 참여 시 버튼 비활성화 기능 추가

--- app/src/main/java/softeer/tenten/EventDetailActivity.kt
+++ app/src/main/java/softeer/tenten/EventDetailActivity.kt
@@ -1,18 +1,22 @@
 package softeer.tenten
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
+import softeer.tenten.dialog.OnDialogResultListener
 import softeer.tenten.fragments.moreInfo.EventDetailBottomSheetFragment
 
-class EventDetailActivity : AppCompatActivity() {
+class EventDetailActivity : AppCompatActivity(), OnDialogResultListener {
+    private lateinit var participateBtn: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_detail)
 
         val bottomSheet = EventDetailBottomSheetFragment()
-        val participateBtn = findViewById<AppCompatButton>(R.id.participateBtn) // 참여 인증 버튼
+        participateBtn = findViewById(R.id.participateBtn) // 참여 인증 버튼
         val completeParticipateBtn =
             findViewById<AppCompatButton>(R.id.completeParticipateBtn) // 참여 완료 버튼
 
@@ -26,14 +30,21 @@ class EventDetailActivity : AppCompatActivity() {
                 bottomSheet.show(supportFragmentManager, "open bottom sheet")
             }
         }
-
-
     }
+
 
     // db에서 참여 인증 여부 확인하기 위한 변수
     fun checkParticipate(): Boolean {
         // db에서 확인하는 코드~~~~~~~~~~~~~~~~~~~
         // ...
         return false;
+    }
+
+    override fun onEventParticipateDialogResult(result: Boolean?) {
+        if(result!!){
+            participateBtn.setTextColor(Color.parseColor("#000000"))
+            participateBtn.isEnabled = false
+            participateBtn.text = "참여 완료"
+        }
     }
 }
